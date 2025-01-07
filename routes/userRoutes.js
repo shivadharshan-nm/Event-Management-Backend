17c90a40a989a68e417fc14a5731194ff5964d1f
shivadharshan-nm <sivadharsan.nm@gmail.com>
2025-01-07 20:22:42 +0530

    backend done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import { registerUser, loginUser, getUserProfile, updateUserProfile, getUsers, getUserById, updateUserById, deleteUserById } from '../controllers/userController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-//router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
-router.get('/profile', protect, getUserProfile);
-router.put('/profile', protect, updateUserProfile);
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+
 // Route to get all users
 router.get('/', protect, getUsers);
 
